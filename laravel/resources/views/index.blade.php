b07a5d8f9716d9e99d4c400bd3a1a4b424969388
Pouloo <pouloohd@outlook.com>
2025-12-09 18:07:49 +0100

    DONE: Imported css/html into laravel views

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="Fr">
+    <head>
+        <meta charset="UTF-8">
+        <link href="style.css" rel="stylesheet">
+		<link href="images/favicon.png" rel="icon" type="image/png">
+        <link href="https://fonts.googleapis.com/css?family=Crete+Round" rel="stylesheet">
+        <title>ThonMayo</title>
+    </head>
+	<body>
+        <header>
+			<ul>
+				<img src="images/logo.png">
+				<li class="burger">
+					<a href="pages/contact.html">Nous Contacter</a>
+					<ul class="sub_burger">
+						<li><a href="#">Sub-Burger Item</a></li>
+						<li><a href="#">Sub-Burger Item</a></li>
+						<li><a href="#">Sub-Burger Item</a></li>
+					</ul>
+				</li>
+				<li class="burger">
+					<a href="pages/engagement.html">Nos Engagements</a>
+					<ul class="sub_burger"> 
+						<li><a href="#">Sub-Burger Item</a></li>
+						<li><a href="#">Sub-Burger Item</a></li>
+						<li><a href="#">Sub-Burger Item</a></li>
+					</ul>
+				</li>
+				<li class="burger">
+					<a href="pages/produits.html">Nos Produits</a>
+					<ul class="sub_burger"> 
+						<li><a href="#">Sub-Burger Item</a></li>
+						<li><a href="#">Sub-Burger Item</a></li>
+						<li><a href="#">Sub-Burger Item</a></li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">Accueil</a>
+				</li>
+			</ul>
+		</header>
+		<section id="overview">
+			<ul>
+				<li id="step1">
+					<div class="steps_overlay">
+						<h4>Choisir</h4>
+						<p>Faites le choix d'une de nos spécialités culinaires.</p>
+					</div>
+				</li>
+				<li id="step2">
+					<div class="steps_overlay">
+						<h4>Commander</h4>
+						<p>Produits à la demande sur notre site ou en boutique.</p>
+					</div>
+				</li>
+				<li id="step3">
+					<div class="steps_overlay">
+						<h4>Déguster</h4>
+						<p>C'est prêt, à table!</p>
+					</div>
+				</li>
+			</ul>
+		</section>
+		<footer>
+			<h1>ThonMayo</h1>
+			<span class="copyright">Copyright © 2025. Tous droits reservés.</span>
+		</footer>
+    </body>
+</html>
